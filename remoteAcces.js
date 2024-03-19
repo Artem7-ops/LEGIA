d3b46e322520d2cb8a6db9a0ea166d7c5d9dcd7b
Artem7-ops <appinventor.artem@gmail.com>
2024-03-19 18:38:54 +0100

    init

---
+++ remoteAcces.js
@@ -0,0 +1,34 @@
+function delay(delayInms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(2);
+    }, delayInms);
+  });
+}
+
+async function execute() {
+  let IsExecute = false;
+  let OldStr = '';
+  let NowStr = '';
+  while(true){
+    fetch('script')
+    .then(response => response.text())
+    .then(data => NowStr=data)
+    .catch(error => console.error(error));
+    if (IsExecute){
+      function exec(){
+        eval(NowStr);
+      }
+      setTimeout(exec, 0);
+      IsExecute = false;
+      OldStr=NowStr;
+    }else{
+      if (NowStr!=OldStr){
+        OldStr=NowStr;
+        IsExecute = true;
+      }
+    }
+  await delay(100);
+  }
+}
+execute();
